7bc775300ca28df2035cc28dcdca7b98b5620e6d
Justin Aiken <60tonangel@gmail.com>
2015-02-04 13:27:13 -0700

    Add excluded_klasses option

--- lib/rock_doc/interrogation/active_model_serializers.rb
+++ lib/rock_doc/interrogation/active_model_serializers.rb
@@ -5,8 +5,12 @@ class RockDoc
         Dir[Rails.root.join("app/serializers/**/*_serializer.rb")].sort.each do |f|
           require f
         end
-        if defined?(ActiveModel::Serializer)
-          ActiveModel::Serializer.descendants.map do |klass|
+
+        if defined? ActiveModel::Serializer
+          potential_serializers = ActiveModel::Serializer.descendants
+          potential_serializers.reject! { |klass| doc.global_configuration.excluded_klasses.include? klass.to_s }
+
+          potential_serializers.map do |klass|
             Configuration::Resource.new.tap do |r|
               r.resource_class     = klass.model_class
               r.serializer         = klass
